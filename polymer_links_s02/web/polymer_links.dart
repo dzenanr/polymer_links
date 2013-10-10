efe951fe1e84d5e27f2983cf66bcff1ea8662d46
dzenanr <dzenanr@gmail.com>
2013-10-10 15:59:59 -0400

    use model and weblinks

--- polymer_links_s02/web/polymer_links.dart
+++ polymer_links_s02/web/polymer_links.dart
@@ -1,15 +1,13 @@
-//import 'dart:html';
+import 'dart:html';
 
-//import 'package:polymer/polymer.dart';
 import 'package:polymer_links/links.dart';
 
-//@published var links = new List<Link>();
 var links = new List<Link>();
 
 main() {
   var link1 = new Link('On Dart', 'http://ondart.me/');
-  var link2 = new Link('Web UI', 'http://www.dartlang.org/articles/web-ui/');
+  var link2 = new Link('Polymer.dart', 'https://www.dartlang.org/polymer-dart/');
   var link3 = new Link('Books To Read', 'http://www.goodreads.com/');
   links..add(link1)..add(link2)..add(link3);
-  //query('#tmpl').model = links;
+  query('#tmpl').model = links;
 }
--- polymer_links_s02/web/web_links.dart
+++ polymer_links_s02/web/web_links.dart
@@ -3,6 +3,6 @@ import 'package:polymer_links/links.dart';
 
 @CustomTag('web-links')
 class WebLinks extends PolymerElement {
-  @published List<Link> webLinks; 
-  //List<Link> webLinks;
+  //@published List<Link> webLinks; 
+  @published List<Link> weblinks; 
 }
